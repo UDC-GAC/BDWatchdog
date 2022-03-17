674d4387b06926939d23b90721caa1903c994426
JonatanEnes <jonatan.enes@udc.es>
2022-03-17 17:23:53 +0100

    Fixes to the scripts to lunch a tmux with atop running

--- MetricsFeeder/scripts/run_atop_stream_tmux.sh
+++ MetricsFeeder/scripts/run_atop_stream_tmux.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
-source "${scriptDir}/../../set_pythonpath.sh"
-tmux new -s "ATOP" "bash $scriptDir/run_atop_stream.sh"
-
-
-
+export scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+tmux new -s "ATOP" -d "bash $scriptDir/run_atop_stream.sh"
