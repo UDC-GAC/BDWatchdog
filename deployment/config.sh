acf4172181c482f73ca211aee14a0e28000f4091
Óscar Castellanos Rodríguez <oscar.castellanos@udc.es>
2022-12-01 10:48:47 +0100

    Changes to allow installation outside HOME directory

--- deployment/config.sh
+++ deployment/config.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-export DATA_DIR=${HOME}/BDWatchdog_data/
+
+if [ -z ${INSTALLATION_PATH} ]
+then
+    INSTALLATION_PATH=${HOME}
+fi
+
+export DATA_DIR=${INSTALLATION_PATH}/BDWatchdog_data/
 #export DATA_DIR=/data/0
-export BDWATCHDOG_DIR=${HOME}/BDWatchdog/
+export BDWATCHDOG_DIR=${INSTALLATION_PATH}/BDWatchdog/
 
